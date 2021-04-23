a299b157376f1aee9541c39904656e8705bef74d
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-04-23 16:19:48 +0530

    done

--- src/assets/Card.jsx
+++ src/assets/Card.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 const Card = (props) => {
     return (
         <>
-            <div className="contianerCard" style={{display:"inline-block"}}>
-                <div class="card mt-2" style={{ width: "18rem", marginLeft:"10rem" }}>
-                    <img src={props.cardImg} class="card-img-top" alt="..." />
-                    <hr />
-                    <div class="card-body">
-                        <h5 class="card-title">{props.cardTitle}</h5>
-                        <p class="card-text">{props.cardSubtitle}</p>
-                    </div>
+            <div class="card mt-2" style={{ width: "18rem", marginLeft: "10rem" }}>
+                <img src={props.cardImg} class="card-img-top" alt="..." />
+                <hr />
+                <div class="card-body">
+                    <h5 class="card-title">{props.cardTitle}</h5>
+                    <p class="card-text">{props.cardSubtitle}</p>
                 </div>
             </div>
         </>
